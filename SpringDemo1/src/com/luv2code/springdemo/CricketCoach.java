27d74ac6d32e88764855af90b10b623130138ace
krishnalokam <siva.lokam4work@gmail.com>
2023-02-02 14:34:36 +0530

    spring ioc

---
+++ SpringDemo1/src/com/luv2code/springdemo/CricketCoach.java
@@ -0,0 +1,11 @@
+package com.luv2code.springdemo;
+
+public class CricketCoach implements Coach {
+
+	@Override
+	public String getDailyWorkout() {
+		
+		return "Spend 30 minutes on running and 30 minutes on bowling";
+	}
+
+}
